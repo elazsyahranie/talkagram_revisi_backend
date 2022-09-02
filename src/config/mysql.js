c1889b2b5eb06c05d1d00dd01720ae1f64e3b476
elazsyahranie <elazaribrahims@gmail.com>
2022-09-02 21:09:18 +0700

    Migrated to SQLite (first step)

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,17 +1,25 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const sqlite3 = require('sqlite3').verbose();
+const dbFile = __dirname + "../../../db/talkagram.db";
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  timezone: process.env.DB_TIMEZONE
-})
+const connection = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE, (err) => {
+    if(err) throw err;
+    console.log("You're now connected")
+});
 
-connection.connect((error) => {
-  if (error) throw error
-  console.log("You're now Connected...")
-})
+// const mysql = require('mysql2')
+// require('dotenv').config()
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   timezone: process.env.DB_TIMEZONE
+// })
+
+// connection.connect((error) => {
+//   if (error) throw error
+//   console.log("You're now Connected...")
+// })
 
 module.exports = connection
